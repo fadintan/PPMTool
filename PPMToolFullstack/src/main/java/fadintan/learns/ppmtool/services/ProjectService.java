b036fb656308be967ec25a8bdc06bdcd0fe23f45
fadintan <fadintan@gmail.com>
2020-04-10 00:27:18 +0700

    Exception for not-unique identifier

--- PPMToolFullstack/src/main/java/fadintan/learns/ppmtool/services/ProjectService.java
+++ PPMToolFullstack/src/main/java/fadintan/learns/ppmtool/services/ProjectService.java
@@ -2,6 +2,7 @@ package fadintan.learns.ppmtool.services;
 
 
 import fadintan.learns.ppmtool.Project.Project;
+import fadintan.learns.ppmtool.exceptions.ProjectIdException;
 import fadintan.learns.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+"' already exist.");
+        }
+
     }
 }
